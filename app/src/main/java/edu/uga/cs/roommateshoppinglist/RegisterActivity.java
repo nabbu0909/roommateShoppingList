99865e3b0a1f65f66a12306564c6d28435fc2566
cschildress <calchildress23@gmail.com>
2020-11-30 16:00:39 -0500

    updated RegisterActivity.java
    added ShoppingList.java & activity_shopping_list.xml
    
    removed activity_shopping_list_management.xml

--- app/src/main/java/edu/uga/cs/roommateshoppinglist/RegisterActivity.java
+++ app/src/main/java/edu/uga/cs/roommateshoppinglist/RegisterActivity.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.firebase.ui.auth.IdpResponse;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -19,9 +20,9 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    private EditText emailEditText;
-    private EditText passwordEditText;
-    private Button registerButton;
+    private EditText email, password, confirmPassword;
+    private Button register;
+    private static final int RC_SIGN_IN = 123;
 
     private static final String DEBUG_TAG = "RegisterActivity";
 
@@ -29,53 +30,73 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        emailEditText = (EditText) findViewById( R.id.editText1 );
-        passwordEditText = (EditText) findViewById( R.id.editText2 );
+        email = (EditText) findViewById( R.id.editText1 );
+        password = (EditText) findViewById( R.id.editText2 );
+        confirmPassword = (EditText) findViewById(R.id.editText);
 
-        registerButton = (Button) findViewById( R.id.button3 );
-        registerButton.setOnClickListener( new RegisterButtonClickListener() );
+        register = (Button) findViewById( R.id.button3 );
+        register.setOnClickListener( new RegisterButtonClickListener() );
     }
 
     private class RegisterButtonClickListener implements View.OnClickListener {
         @Override
         public void onClick(View view) {
-            final String email = emailEditText.getText().toString();
-            final String password = passwordEditText.getText().toString();
 
             final FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
-            // This is how we can create a new user using an email/password combination.
-            // Note that we also add an onComplete listener, which will be invoked once
-            // a new user has been created by Firebase.  This is how we will know the
-            // new user creation succeeded or failed.
-            // If a new user has been created, Firebase already signs in the new user;
-            // no separate sign in is needed.
-            mAuth.createUserWithEmailAndPassword( email, password )
-                    .addOnCompleteListener( RegisterActivity.this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
+            if(!(password.getText().toString().equalsIgnoreCase(password.getText().toString()))){
+                Toast.makeText(RegisterActivity.this, "Passwords must be the same", Toast.LENGTH_LONG).show();
+            } else {
 
-                                Toast.makeText( getApplicationContext(),
-                                        "Registered user: " + email,
-                                        Toast.LENGTH_SHORT ).show();
+                // This is how we can create a new user using an email/password combination.
+                // Note that we also add an onComplete listener, which will be invoked once
+                // a new user has been created by Firebase.  This is how we will know the
+                // new user creation succeeded or failed.
+                // If a new user has been created, Firebase already signs in the new user;
+                // no separate sign in is needed.
+                mAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                        .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
 
-                                // Sign in success, update UI with the signed-in user's information
-                                Log.d( DEBUG_TAG, "createUserWithEmail: success" );
+                                    Toast.makeText(getApplicationContext(),
+                                            "Registered user: " + email,
+                                            Toast.LENGTH_SHORT).show();
 
-                                FirebaseUser user = mAuth.getCurrentUser();
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d(DEBUG_TAG, "createUserWithEmail: success");
 
-                                Intent intent = new Intent( RegisterActivity.this, MainActivity.class );
-                                startActivity( intent );
+                                    FirebaseUser user = mAuth.getCurrentUser();
 
-                            } else {
-                                // If sign in fails, display a message to the user.
-                                Log.w(DEBUG_TAG, "createUserWithEmail: failure", task.getException());
-                                Toast.makeText(RegisterActivity.this, "Registration failed.",
-                                        Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                                    startActivity(intent);
+
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w(DEBUG_TAG, "createUserWithEmail: failure", task.getException());
+                                    Toast.makeText(RegisterActivity.this, "Registration failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
                             }
-                        }
-                    });
+                        });
+            } //else
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RC_SIGN_IN){
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if(resultCode == RESULT_OK){
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                //for now when a user registers, it takes you back to the main page where you can then sign in w/ new account
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+            } else {
+                Toast.makeText(getApplicationContext(), "Unable to sign in", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 
---
+++ app/src/main/java/edu/uga/cs/roommateshoppinglist/ShoppingList.java
@@ -0,0 +1,45 @@
+package edu.uga.cs.roommateshoppinglist;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ShoppingList extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    Button addShoppingListItem, viewPurchasedItems;
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_list);
+
+        recyclerView = findViewById(R.id.shoppingListRecyclerView);
+        addShoppingListItem = findViewById(R.id.addNewItemButton);
+        addShoppingListItem.setOnClickListener(new AddShoppingListItemButtonClickListener());
+        viewPurchasedItems = findViewById(R.id.viewPurchasedItemsButton);
+        viewPurchasedItems.setOnClickListener(new PurchasedItemsButtonClickListener());
+    }
+
+    private class PurchasedItemsButtonClickListener implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            //Intent intent = new Intent(ShoppingList.this, TBD.class);
+            //ShoppingList.this.startActivity(intent);
+        }
+    }
+
+    private class AddShoppingListItemButtonClickListener implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            //Intent intent = new Intent(ShoppingList.this, TBD.class);
+            //ShoppingList.this.startActivity(intent);
+        }
+    }
+
+}
