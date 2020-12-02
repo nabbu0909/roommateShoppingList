1d13df9eaff7812cb64287bb963344ce16256ad6
nabbu0909 <navam.narula@gmail.com>
2020-12-02 15:56:16 -0500

    Finished shopping list recyvler adaptor

---
+++ app/src/main/java/edu/uga/cs/roommateshoppinglist/ShoppingItem.java
@@ -0,0 +1,19 @@
+package edu.uga.cs.roommateshoppinglist;
+
+public class ShoppingItem {
+
+    private String name;
+
+    public ShoppingItem(String name) {
+        this.name = name;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- app/src/main/java/edu/uga/cs/roommateshoppinglist/ShoppingList.java
+++ app/src/main/java/edu/uga/cs/roommateshoppinglist/ShoppingList.java
@@ -5,13 +5,29 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShoppingList extends AppCompatActivity {
 
+    final FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference referenceItems = database.getReference("ShoppingList");
+    private List<ShoppingItem> items = new ArrayList<ShoppingItem>();
+
     private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private RecyclerView.Adapter recyclerAdapter;
     Button addShoppingListItem, viewPurchasedItems;
 
     public static final String DEBUG_TAG = "ShoppingList";
@@ -20,14 +36,22 @@ public class ShoppingList extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_list);
 
-        recyclerView = findViewById(R.id.shoppingListRecyclerView);
+        items.add(new ShoppingItem("Eggs"));
+        items.add(new ShoppingItem("Bacon"));
+
+        //create recycler
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView = (RecyclerView) findViewById(R.id.shoppingListRecyclerView);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerAdapter = new ShoppingListRecyclerAdapter( items );
+        recyclerView.setAdapter( recyclerAdapter );
+
+        //add buttons
         addShoppingListItem = findViewById(R.id.addNewItemButton);
         addShoppingListItem.setOnClickListener(new AddShoppingListItemButtonClickListener());
         viewPurchasedItems = findViewById(R.id.viewPurchasedItemsButton);
         viewPurchasedItems.setOnClickListener(new PurchasedItemsButtonClickListener());
 
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(linearLayoutManager);
     }
 
     private class PurchasedItemsButtonClickListener implements View.OnClickListener{
@@ -46,6 +70,26 @@ public class ShoppingList extends AppCompatActivity {
             //Intent intent = new Intent(ShoppingList.this, TBD.class);
             //ShoppingList.this.startActivity(intent);
         }
+    }
+
+    public void readItems() {
+        referenceItems.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                items.clear();
+                List<String> keys = new ArrayList<>();
+                for(DataSnapshot key : snapshot.getChildren()){
+                    keys.add(key.getKey());
+                    ShoppingItem item = key.getValue(ShoppingItem.class);
+                    items.add(item);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
 }
